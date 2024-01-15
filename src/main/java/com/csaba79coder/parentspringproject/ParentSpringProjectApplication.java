ba475e76ca3f80b1c204c853544a4e20a4b368ca
webler-biology-physiology <csabavadasz79@gmail.com>
2024-01-15 20:30:18 +0100

    Initial commit

---
+++ src/main/java/com/csaba79coder/parentspringproject/ParentSpringProjectApplication.java
@@ -0,0 +1,13 @@
+package com.csaba79coder.parentspringproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParentSpringProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParentSpringProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/csaba79coder/parentspringproject/ParentSpringProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.csaba79coder.parentspringproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParentSpringProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
